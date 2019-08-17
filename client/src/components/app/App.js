ef7ad8c77447dca7e195cff54d58ae7313495a36
briantam23 <briantam23@gmail.com>
2019-08-17 14:52:33 -0400

    Cleaned up homepage

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import Homepage from '../homepage/Homepage';
 import Profile from '../profile/Profile';
@@ -7,10 +7,10 @@ import Profile from '../profile/Profile';
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-        <h1>Sebra</h1>
-    </header>
     <Router>
+      <header className="App-header">
+        <Link to='/'>Sebra</Link>
+      </header>
       <Route path='/' render={ ({ history }) => <Homepage history={ history }/> }/>
       <Route path='/profile' render={ () => <Profile/> }/>
     </Router>
--- client/src/components/homepage/Homepage.js
+++ client/src/components/homepage/Homepage.js
@@ -5,7 +5,9 @@ import { Button } from 'reactstrap';
 
 const Homepage = ({ history }) => (
   <div className="Homepage">
-    <Button onClick={ () => history.push('/profile') } color='info'>Pay with Libra</Button>
+    <Button onClick={ () => history.push('/profile') } color='primary'>
+        Pay with Libra
+    </Button>
   </div>
 )
 
