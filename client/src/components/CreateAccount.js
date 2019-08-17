ce7ea5bf9cfd830991b8dd40819a1a759bf66c70
briantam23 <briantam23@gmail.com>
2019-08-17 18:16:16 -0400

    Cleaning up React files

--- client/src/components/CreateAccount.js
+++ client/src/components/CreateAccount.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './CreateAccount.css';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import './Homepage.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 
 const useStyles = makeStyles(theme => ({
+  homepage: {
+    margin: 0,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  },
   button: {
     margin: theme.spacing(1),
+    fontSize: '30px',
+    padding: '20px 40px'
   },
   input: {
     display: 'none',
@@ -17,7 +25,7 @@ const useStyles = makeStyles(theme => ({
 const Homepage = ({ history }) => {
   const classes = useStyles();
   return (
-    <div className="Homepage">
+    <div className={classes.homepage}>
       <Button onClick={ () => history.push('/create-account') } variant="contained" color="primary" className={classes.button}>
           Pay with Libra
       </Button>
