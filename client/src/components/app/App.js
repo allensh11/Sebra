5dca376f579b8e3420de93c390c789e21e10bb40
briantam23 <briantam23@gmail.com>
2019-08-24 13:21:55 -0400

    Added Nav

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
-import Homepage from '../Homepage';
+//import Homepage from '../Homepage';
+import Nav from '../Nav';
 import CreateAccount from '../CreateAccount';
 import Account from '../Account';
 import CompletedPayment from '../CompletedPayment';
@@ -8,7 +9,8 @@ import CompletedPayment from '../CompletedPayment';
 
 const App = () => (
   <Router>
-    <Route exact path='/' render={ ({ history }) => <Homepage history={ history }/> }/>
+    {/* <Route exact path='/' render={ ({ history }) => <Homepage history={ history }/> }/> */}
+    <Nav/>
     <Route path='/create-account' render={ ({ history }) => <CreateAccount history={ history }/> }/>
     <Route exact path='/account/:customerId' render={ ({ match, history }) => <Account customerId={ match.params.customerId } history={ history }/> }/>
     <Route path='/account/:customerId/completed' render={ ({ history }) => <CompletedPayment history={ history }/> }/>
