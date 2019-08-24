aead15bff71a9b76d4a51fe3d5a5f0f8ed62f58d
Allen <allen@lovecolour.co.nz>
2019-08-25 01:29:29 +1200

    API updates - account creation/minting/balance checks.

--- api/sebra/__init__.py
+++ api/sebra/__init__.py
@@ -2,7 +2,10 @@ import os
 import shelve
 from flask import Flask, g
 from flask_restful import Resource, Api, reqparse
-from libra_actions import account, balance, mint, qr_create, getAccount
+from libra_actions import account, balance, mint, transfer
+import time
+import json
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -30,42 +33,69 @@ class LibraAccount(Resource):
     def get(self):
         shelf = get_db()
         keys = list(shelf.keys())
-        wallets = []
+        accounts = []
         for key in keys:
-            ac_details = shelf[key]
-            mnemonic2 = ac_details['mnemonic'] #throws error...why?
-        return {'message': 'Success', 'data': ac_details['mnemonic']} #doesn't...
-    
-        shelf = get_db()
-        newWallet = account()
-        shelf[newWallet['address']] = newWallet
-        return {'message': 'Success - wallet created', 'data': newWallet}
+            account = shelf[key]
+            #TODO: Can be sped up by not worrying about returning balance??
+            account['balance'] = balance(account['address'])
+            accounts.append(account)
+        return {'message': 'Success', 'data': accounts} 
 
     #registering new user
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('id', required=True)
         parser.add_argument('username', required=True)
         parser.add_argument('password', required=True)
-        parser.add_argument('mnemonic', required=True)
         args = parser.parse_args()
-        shelf = get_db()
-        shelf[args['id']] = args
-        #Mint new Libra by Mnemonic
-        mint(args['mnemonic'], 1000)
-        return {'message': 'Wallet added', 'data': getAccount(args['mnemonic'])}, 201
 
-class LibraWallet(Resource):
-    def transferBal(self):
-        return {'message': 'Transferred', 'data': getAccount(args['mnemonic'])}, 201
-    #Can a wallet be destroyed from the testnet? Or just from our DB...
-    def delete(self, id):
+        #TODO: password to be hashed...
+        #TODO: How should we go about private key storage?
+        newWallet = account()
+
+        args['mnemonic'] = newWallet['mnemonic']
+        args['address'] = newWallet['address']
+        args['public_key'] = newWallet['public_key']
+        args['private_key'] = newWallet['private_key']
+
         shelf = get_db()
-        if not (id in shelf):
-            return {'message': 'wallet not found', 'data': {}}, 404
-        del shelf[id]
-        return '', 204
+        shelf[args['address']] = args
+
+        #Mint new libra by mnemonic
+        mint(args['mnemonic'], 1000)
+        return {'message': 'Wallet added', 'data': account(args['mnemonic'])}, 201
+
+class LibraTransaction(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('senderMnemonic', required=True)
+        parser.add_argument('recipientMnemonic', required=True)
+        parser.add_argument('amount', required=True)
+        args = parser.parse_args()
+        resp = transfer(args['senderMnemonic'], args['recipientMnemonic'], args['amount'])
+        #ret = resp['id']
+        return {'message': 'Transfer complete', 'data': 'ret'}, 200
+
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('recipientMnemonic', required=True)
+        parser.add_argument('amountToMint', required=True)
+        args = parser.parse_args()
+        mint(args['recipientMnemonic'], args['amountToMint'])
+        return {'message': 'Minted'}, 200
+
+
+
+# class LibraWallet(Resource):
+#     def transferBal(self):
+#         return {'message': 'Transferred', 'data': account(args['mnemonic'])}, 201
+#     #Can a wallet be destroyed from the testnet? Or just from our DB...
+#     def delete(self, id):
+#         shelf = get_db()
+#         if not (id in shelf):
+#             return {'message': 'wallet not found', 'data': {}}, 404
+#         del shelf[id]
+#         return '', 204
         
-api.add_resource(LibraOperations, '/libra')
-api.add_resource(LibraMinter, '/mint')
-api.add_resource(LibraWallet, '/libra/<string:mnemonic>')
+api.add_resource(LibraAccount, '/libra')
+api.add_resource(LibraTransaction, '/transactions')
+#api.add_resource(LibraWallet, '/libra/<string:mnemonic>')
