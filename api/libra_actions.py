d3764ecf5cbaf2a150bb7999b8945a01357859ca
Allen <allen@lovecolour.co.nz>
2019-08-29 00:14:09 +1200

    api updates - payer end

--- api/libra_actions.py
+++ api/libra_actions.py
@@ -24,23 +24,6 @@ def account(words=None):
     }
     return account_details
 
-def accountByIndex(index, words=None):
-    wallet = LibraWallet() if words is None else LibraWallet(words)
-    account = wallet.get_account(int(index))
-    if client.get_account_state(account) == None or round(client.get_account_state(account).balance/1000000) == 0:
-        balance = 0
-    else:
-        balance = round(client.get_account_state(account).balance/1000000)
-    
-    account_details = {
-        'address': account.address,
-        'mnemonic': wallet.to_mnemonic(),
-        'public_key': account.public_key,
-        'private_key': account.private_key,
-        'balance': balance
-    }
-    return account_details
-
 def balance(account):
     account_state = client.get_account_state(account)
     if client.get_account_state(account) == None or round(client.get_account_state(account).balance/1000000) == 0:
@@ -55,18 +38,16 @@ def mint(mnemonic, amount):
     client.mint_with_faucet(account, int(amount)*1000000)
     return balance(account)
     
-def transfer(senderMnemonic, recipientMnemonic, amount):
+def transfer(senderMnemonic, recipientAddress, amount):
     senderWallet = LibraWallet(senderMnemonic)
-    recipientWallet = LibraWallet(recipientMnemonic)
 
     senderAccount = senderWallet.get_account(0)
-    recipientAccount = recipientWallet.get_account(0)
     # Create a transfer transaction object to send 0.001 Libra to account2
-    tx = TransferTransaction(recipientAccount.address, int(amount)*1000000)
+    tx = TransferTransaction(recipientAddress, int(amount)*1000000)
     # Or to send to a plain hex address
 
     # You can send a transaction by calling `send_transaction` function, which takes a sender `Account` and a `Transaction` object. You can also optionally passed `max_gas_amount`, `gas_unit_price`, and `expiration_time`. 
-    ret = client.send_transaction(senderAccount, tx, max_gas_amount=10000000, gas_unit_price=100, expiration_time=1566655079)
+    ret = client.send_transaction(senderAccount, tx, max_gas_amount=10000000, gas_unit_price=100, expiration_time=1866655079)
     return ret 
 
 def qr_create(address):
--- api/sebra/__init__.py
+++ api/sebra/__init__.py
@@ -1,13 +1,10 @@
-import os
-import shelve
-from flask import Flask, g
+import os, shelve, json, hashlib
+from flask import Flask, g, session, request
 from flask_restful import Resource, Api, reqparse
 from libra_actions import account, balance, mint, transfer
-import time
-import json
-
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 api = Api(app)
 
 def get_db():
@@ -22,80 +19,101 @@ def teardown_db(exception):
     if db is not None:
         db.close()
 
+#Return our react page?
 @app.route("/")
 def index():
     content = "Ok"
     return content
 
-
-class LibraAccount(Resource):
-    #Getting all wallets...
-    def get(self):
-        shelf = get_db()
+#-----------REGISTER-----------
+#req params: 
+#   -username
+#   -password
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    shelf = get_db()
+    if(request.method == 'GET'):
+        #HACK: Just gets all accounts. We should delete this later.
         keys = list(shelf.keys())
         accounts = []
         for key in keys:
-            account = shelf[key]
-            #TODO: Can be sped up by not worrying about returning balance??
-            account['balance'] = balance(account['address'])
-            accounts.append(account)
-        return {'message': 'Success', 'data': accounts} 
+            accountFromDb = shelf[key]
+            accountFromDb['balance'] = balance(accountFromDb['address'])
+            accounts.append(accountFromDb)
+        ret = {'message': 'Success', 'data': accounts}
+        return json.dumps(ret)
+    elif(request.method == 'POST'):
+        #POST: registering new      
+        if(request.form.get('username') in shelf):
+            return json.dumps({'message': 'error', 'data': 'User already registered.'})
 
-    #registering new user
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('username', required=True)
-        parser.add_argument('password', required=True)
-        args = parser.parse_args()
-
-        #TODO: password to be hashed...
-        #TODO: How should we go about private key storage?
-        newWallet = account()
-
-        args['mnemonic'] = newWallet['mnemonic']
-        args['address'] = newWallet['address']
-        args['public_key'] = newWallet['public_key']
-        args['private_key'] = newWallet['private_key']
-
-        shelf = get_db()
-        shelf[args['address']] = args
-
+        ret = account()
+        ret['password'] = hashlib.md5(request.form.get('password').encode('utf-8')).hexdigest()
+        ret['username'] = request.form.get('username')
+        ret['type'] = "customer"
+        shelf[ret['username']] = ret
         #Mint new libra by mnemonic
-        mint(args['mnemonic'], 1000)
-        return {'message': 'Wallet added', 'data': account(args['mnemonic'])}, 201
-
-class LibraTransaction(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('senderMnemonic', required=True)
-        parser.add_argument('recipientMnemonic', required=True)
-        parser.add_argument('amount', required=True)
-        args = parser.parse_args()
-        resp = transfer(args['senderMnemonic'], args['recipientMnemonic'], args['amount'])
-        #ret = resp['id']
-        return {'message': 'Transfer complete', 'data': 'ret'}, 200
-
-    def put(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('recipientMnemonic', required=True)
-        parser.add_argument('amountToMint', required=True)
-        args = parser.parse_args()
-        mint(args['recipientMnemonic'], args['amountToMint'])
-        return {'message': 'Minted'}, 200
+        mintamount = 1000
+        mint(ret['mnemonic'], mintamount)
+        session['user'] = ret['username']
+        return json.dumps({'message': 'success', 'data': account(ret['mnemonic'])})
 
 
+#-----------LOG IN-----------
+# req params: 
+#   -username
+#   -password
+@app.route('/login', methods=['GET'])
+def login():
+    reqParsed = {}
+    username =  request.args.get('username')
+    password =  request.args.get('password')
+    shelf = get_db()
+    if(username not in shelf):
+        return json.dumps({'message': 'error', 'data': 'User not registered.'})
+    passStored = shelf[username]['password']
+    passProvided = hashlib.md5(password.encode('utf-8')).hexdigest()
+    if(passStored == passProvided):
+        session['user'] = username
+        return json.dumps({'message': 'success', 'data': account(shelf[username]['mnemonic'])})
+    else:
+        return json.dumps({'message': 'Not authorized'})
 
-# class LibraWallet(Resource):
-#     def transferBal(self):
-#         return {'message': 'Transferred', 'data': account(args['mnemonic'])}, 201
-#     #Can a wallet be destroyed from the testnet? Or just from our DB...
-#     def delete(self, id):
-#         shelf = get_db()
-#         if not (id in shelf):
-#             return {'message': 'wallet not found', 'data': {}}, 404
-#         del shelf[id]
-#         return '', 204
-        
-api.add_resource(LibraAccount, '/libra')
-api.add_resource(LibraTransaction, '/transactions')
-#api.add_resource(LibraWallet, '/libra/<string:mnemonic>')
+
+#-----------LOG OUT-----------
+#req params: None
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('user', None)
+    return json.dumps({'message': 'success', 'data': 'logged out'})
+
+
+#-----------TRANSACTIONS-----------
+#req params:
+#   -recipientAddress
+#   -amount
+#   -senderUsername
+@app.route('/transaction', methods=['GET'])
+def transaction():
+
+   
+
+    recipientAddress =  request.args.get('recipientAddress')
+    amount =  request.args.get('amount')
+    senderUsername=  request.args.get('senderUsername')
+    shelf = get_db()
+
+    if ('user' in session and session['user'] == senderUsername):
+        senderMnemonic = shelf[senderUsername]['mnemonic']
+        resp = transfer(senderMnemonic, recipientAddress, amount)
+        return json.dumps({'message': 'Success', 'data': 'Transferred ' + amount + 'to ' + recipientAddress})
+    else :
+        return json.dumps({'message': 'Not authorized'})
+
+
+#Helper method to pass requests.
+def parseRequest(requestObj):
+    reqParsed = {}
+    for key, value in requestObj:
+        requestObj[ key ] = value
+    return reqParsed
